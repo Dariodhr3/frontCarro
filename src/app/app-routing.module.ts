6feb4c07f22252eccaad02c2da4a9b23bf6c0b8c
Your Name <you@example.com>
2019-04-14 12:35:49 -0500

    my proyect is working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
+import { AppComponent } from './app.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'/appComponent', pathMatch: 'full'},
+  {path:'appComponent', component: AppComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
